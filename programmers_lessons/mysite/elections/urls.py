8e2802c78b236894a082308078b00da716cddcd2
jincrates <94jingyu@gmail.com>
2021-09-16 22:08:09 +0900

    model 추가

--- programmers_lessons/mysite/elections/urls.py
+++ programmers_lessons/mysite/elections/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views #.은 현재폴더의 디렉토리라는뜻. 즉 현재폴더의 views.py를 import하는것임
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index),  #위의 urls.py와는 달리 include가 없음
 ]
