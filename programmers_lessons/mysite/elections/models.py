62d1069aed82872b2440cc0eba9c9abbf3f9971b
jincrates <94jingyu@gmail.com>
2021-09-16 22:15:30 +0900

    마이그레이션과 DB

--- programmers_lessons/mysite/elections/models.py
+++ programmers_lessons/mysite/elections/models.py
@@ -5,5 +5,5 @@ class Candidate(models.Model):
     name = models.CharField(max_length = 10)
     introduction = models.TextField()
     area = models.CharField(max_length = 15)
-    party_number = models.IntegerField(default = 1)
+    party_number = models.IntegerField(default = 0)
 
--- programmers_lessons/mysite/readme.md
+++ programmers_lessons/mysite/readme.md
@@ -1,2 +1,8 @@
-
+# 장고 서버 실행
 $ python3 manage.py runserver
+
+
+# 모델을 DB에 저장하기 위한 준비과정
+$ python3 manage.py makemigrations
+
+$ python3 manage.py migrate로 DB에 공간 만들기
